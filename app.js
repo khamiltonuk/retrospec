8168cb085d9e1abe0176f5951581af1b42b6aa63
khamiltonuk <krisham87@hotmail.com>
2015-03-11 08:44:43 +0000

    set up static files served via node

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 'use strict';
 
-var app = require('express')(),
+var express = require('express'),
+    app = require('express')(),
     favicon = require('serve-favicon'),
-    http = require('http').Server(app);
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
 
-    /**
-    * serve favicon
-    */
-    app.use(favicon(__dirname + '/assets/favicons/favicon.ico'));
+/**
+* serve favicon
+*/
+app.use(favicon(__dirname + '/assets/favicons/favicon.ico'));
+
+/**
+* declare static folder
+* this forwards requests to static files (css, js, images)
+* to the /assets directory.
+*
+* with this, you never need to include /assets/ in your url
+* when referencing static files.
+*/
+app.use(express.static(__dirname + '/assets'));
 
 
 
