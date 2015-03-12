28119bb33a39f88c7fd4b2806695adb7687ae2b8
khamiltonuk <krisham87@hotmail.com>
2015-03-12 09:14:54 +0000

    now running on grunt js and pre compiled css

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        // imagemin: {
+        //     dynamic: {                         // Another target
+        //         files: [{
+        //             expand: true,                  // Enable dynamic expansion
+        //             cwd: 'assets/images/',         // Src matches are relative to this path
+        //             src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        //             dest: 'assets/images/dist/'    // Destination path prefix
+        //         }]
+        //     }
+        // },
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        express: {
+            dev: {
+                options: {
+                    script: 'app.js'
+                }
+            },
+        },
+        uglify: {
+            options: {
+                //beautify: true
+            },
+            plugin_files: {
+                files: {
+                    'assets/js/plugins.js':  [
+                    'assets/js/*.js'
+                    ]
+                }
+            },
+            init_files: {
+                files: {
+                    'assets/js/init.min.js':  [
+                    'assets/js/init.js'
+                    ]
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'assets/css/styles.css': 'assets/scss/styles.scss'
+                },
+                options: {
+                    style: 'compressed'
+                }
+            },
+            dev: {
+                files: {
+                    'assets/css/styles.css': 'assets/scss/styles.scss'
+                }
+            }
+        },
+
+        watch: {
+            uglify:  {
+                files: ['assets/js/init.js'],
+                task: ['uglify']
+            },
+            sass: {
+                files: ['assets/scss/*.scss'],
+                tasks: ['sass:dev'],
+                options: {
+                    spawn: false
+                }
+            },
+            express: {
+                files: ['app/**/*.js', 'app.js'],
+                tasks: ['express:dev'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+    });
+
+    //grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    //grunt.registerTask('images', ['imagemin']);
+    grunt.registerTask('compile', ['sass:dist', 'uglify']);
+    grunt.registerTask('serve', ['express:dev', 'uglify', 'sass:dev', 'watch']);
+};
--- assets/js/init.js
+++ assets/js/init.js
@@ -1,1 +1,56 @@
-var socket = io();
+(function(){
+
+    'use strict';
+    var username = "",
+        retroMembers = [],
+        $panels = $('.panel'),
+        $overlay = $('.overlay');
+
+
+    var socket = io();
+
+    //getting the username
+    $('#capture-username').on('submit', function(e){
+        username = $('#m').val().trim();
+        //exsistingUser = retroMembers[username].length;
+        if (username !== "" ){//} && exsistingUser !== -1){
+            closeOverlay();
+            init();
+        }else{
+            $(this).addClass('error');
+            $('.error-messsage').removeClass('hidden').attr('aria-hidden', 'false');
+        }
+
+        e.preventDefault();
+    });
+
+    var init = function(){
+        renderUsername();
+    };
+
+    var renderUsername = function(){
+        $('.username span').text(username);
+    }
+
+    //Retroboard
+    var matchPanelHeight = function(){
+        $panels.height('auto');
+        maxHeight = 0;
+        $panels.each(function(){
+            if ($(this).outerHeight() > maxHeight){
+                maxHeight = $(this).outerHeight();
+            }
+        });
+        $panels.height(maxHeight);
+    };
+
+    //Utills
+    var openOverlay = function(){
+        $overlay.removeClass('hidden').attr('aria-hidden', 'true');
+    };
+
+    var closeOverlay = function(){
+        $overlay.addClass('hidden').attr('aria-hidden', 'false');
+    };
+
+})();
