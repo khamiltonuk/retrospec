e8b84249570f8c6f73ac948f3b77ea7034a6734c
khamiltonuk <krisham87@hotmail.com>
2015-03-13 23:10:58 +0000

    fancy username animation, better styles and first use of clear fix

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,13 +4,21 @@
     var username = "",
         retroMembers = [],
         $panels = $('.panel'),
-        $overlay = $('.overlay');
+        $overlay = $('.overlay'),
+        uniqueId = 0,
+        bankForm = '<li class="draft" draggable="true"><form id="blankItem" >' +
+        '<input type="text" name="item" value="test"/>' +
+        '<button type="submit" class="submit">Submit</button>' +
+        //'<button class="edit hidden" aria-hidden="true">Edit</button>' +
+        '<button class="delete hidden" aria-hidden="true" >delete</button>' +
+        '</form></li>';
 
 
     var socket = io();
 
     //getting the username
     $('#capture-username').on('submit', function(e){
+        e.preventDefault();
         username = $('#m').val().trim();
         //exsistingUser = retroMembers[username].length;
         if (username !== "" ){//} && exsistingUser !== -1){
@@ -21,21 +29,58 @@
             $('.error-messsage').removeClass('hidden').attr('aria-hidden', 'false');
         }
 
-        e.preventDefault();
+
     });
+    $('#m').on('focus', function(){
+        $(this).siblings('label').addClass('active-input');
+        $(this).attr('placeholder','')
+    });
+    $('#m').on('blur', function(){
+        $(this).siblings('label').removeClass('active-input');
+        $(this).attr('placeholder','username')
+    })
+
 
     var init = function(){
         renderUsername();
+        insertDraft();
+
+
+        $('#retro-board').on('blur','.panel input', blurInput );
+        $('#retro-board').on('keyup', 'input', activeItem );
+    };
+
+    var blurInput = function(e){
+        inputValue = $(this).val();
+        if(inputValue === ""){
+            $(this).parent().parent().remove();
+        }
+    };
+
+    var activeItem = function(){
+        $(this).parent().parent().removeClass('draft');
+        insertDraft();
+    };
+
+    var insertDraft = function(){
+        $panels.each(function(){
+            uniqueId += 1;
+            var draftCount = $(this).find('ul > li.draft').length;
+            if(draftCount < 1){
+                $(this).find('ul').append($(bankForm).attr('id', "item-"+ uniqueId ));
+            }
+        });
+        matchPanelHeight();
     };
 
     var renderUsername = function(){
         $('.username span').text(username);
-    }
+    };
 
     //Retroboard
     var matchPanelHeight = function(){
+        var maxHeight = 0;
         $panels.height('auto');
-        maxHeight = 0;
         $panels.each(function(){
             if ($(this).outerHeight() > maxHeight){
                 maxHeight = $(this).outerHeight();
